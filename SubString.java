05b6c6eea71ba9aec703a5a409e5ec446e9efae2
zaki-hashem-chow <zaki.hashim.chow@live.com>
2015-07-09 12:21:06 +0600

    updated on 09-Jul-2015, 12:18 pm. Making subStrings and adding both subString and operators successfully. But order of addition of substrings and operators  to subStringArray is not as expected.

--- SubString.java
+++ SubString.java
@@ -6,89 +6,172 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SubString extends CharType{
-    
-    private static String expression = null;   
-    private static String str= "";   
+   
+   
+       
     private static ArrayList<String> charTypeArray = new ArrayList<String>();
     private static ArrayList<String> charArray = new ArrayList<String>();
     public static ArrayList<String> subStringArray = new ArrayList<String>();
+    static String subStringText;
+    private int i;
+    private String chr1 = null;
     
     
+    private boolean sameOperator(){
+        boolean sameOperator = false;
+        if(charTypeArray.get(i + 1) == null){
+            sameOperator = false;
+        }else if(charTypeArray.get(i + 1) == "operator" || charTypeArray.get(i - 1) == "operator"){        
+            sameOperator = true;
+        }else{
+            sameOperator = false;
+        }
+        return sameOperator;
+    }
+       
     
     private void arrayMaker(){
-       
+        String expression = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Calculation String then press Return: ");        
         expression = sc.nextLine();
         expression = expression.replaceAll(" ", "");     
         CharType ct = new CharType();
         
-        for(int i =0; i<expression.length(); i++){
+        for(i =0; i<expression.length(); i++){
             charTypeArray.add(i, ct.identifyChar(expression.charAt(i)));                                   
             charArray.add(i, Character.toString(expression.charAt(i)));
         }
-    
-    }
-    public ArrayList getCharArray(){
         
-        return charArray;
-    }
-    public ArrayList getCharTypeArray(){
-        return charTypeArray;
     }
     
-    public ArrayList subStringMaker(){
-        String chr1 = null;
-        String subString = null;
+    public ArrayList subStringMaker(ArrayList charTypeArray, ArrayList charArray){
+        
+        charTypeArray = SubString.charTypeArray;
+        charArray = SubString.charArray;        
         String chrType = null;
-        for(int i=0; i<charArray.size(); i++){           
+        String str; 
+        String subStringText = "";
+        
+        FOR:
+        
+        for(i=0; i<charArray.size(); i++){           
             int j=0;   
-            chr1 = charArray.get(i);
-            chrType = charTypeArray.get(i);
+            chr1 = charArray.get(i).toString();
+            chrType = charTypeArray.get(i).toString();
+            
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            if(charTypeArray.get(i) != null){
-               if(charTypeArray.get(i-1) != null){
-                   
-               
-                   
-                    
-               }else{
-                    switch(chrType){
+            IF:
+            
+            if(charArray.size()-i < 2){                 
+                switch(chrType){
                         case "digit": {
-                            subString = chr1;                                                                      
-                            break;}
+                            if(charTypeArray.get(i-1)=="digit"){
+                                subStringText = subStringText + "" + chr1;
+                                subStringArray.add(j, subStringText);
+                                break FOR;
+                                
+                             }else if(charTypeArray.get(i-1)=="operator"){
+                                subStringText = chr1;
+                                subStringArray.add(j, subStringText);
+                                break FOR;
+                             }
+                             break FOR;}
                         
                         case "operator": {
-                            if(chr1.contains("+")){
-                            
-                            }else if(chr1.contains("-")){
-                                             
-                            }else{
-                                  System.out.println("Invalid Operator at first character of string!!");
-                            }break;}
-                        
+                            System.out.println("Invalid Last Craracter!!");
+                             break FOR; }
+                       
                         default: System.out.println("Invalid First Craracter!!");
-                        }
+                            break FOR;
+                }
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::                 
+
+            }else{ 
+                if(charArray.size() >= 1){
                     
-               }
-            }else{
-                System.out.println("i = null!!");
-            }
-            subStringArray.add(str)
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-       }
+                    if(i > 0){
+                        switch(chrType){
+                           case "digit": {
+                               
+                                if(charTypeArray.get(i-1)=="digit"){
+                                    subStringText = subStringText + "" + chr1; 
+                                    break IF;
+                                    
+                                }else if(charTypeArray.get(i-1)=="operator"){
+                                    j++;
+                                    subStringText = subStringText + "" + chr1;
+                                    break IF;
+                                    
+                                }break IF;}
+
+                           case "operator": {
+                               if(chr1.contains("-")){ 
+                                   
+                                    if(sameOperator() == false){
+                                       subStringArray.add(j, subStringText);
+                                       j++;
+                                       subStringArray.add(j, chr1);
+                                       subStringText = "";
+                                       
+                                    }else{
+                                        System.out.println("Invalid Double Operator!!");
+                                        break FOR;
+                                    }  
+                                    break IF;
+                                    
+                               }else if(chr1.contains("+")){
+                                   
+                                    if(sameOperator() == false){
+                                    subStringArray.add(j, subStringText);
+                                    j++;
+                                    subStringArray.add(j, chr1);
+                                    subStringText = "";
+                                    
+                                 }else{
+                                     System.out.println("Invalid Double Operator!!");
+                                     break FOR;
+                                 }
+                                    break IF;
+                               }                          
+                               break IF; } 
+                           
+                           default: System.out.println("Invalid First Craracter!!");
+                               break FOR;
+                           } 
+                    }else{
+                        
+                        switch(chrType){
+                            case "digit": {                           
+                                    subStringText = chr1;                                                                                        
+                                 break IF;} 
+
+                            case "operator": {                           
+                                System.out.println("Invalid First Craracter!!");     //////change this portion to anable operator at 1st character of string
+                                 break FOR; }  
+
+                        default: System.out.println("Invalid First Craracter!!");
+                            break FOR;                                               
+                        } 
+                    }
+                }else{ 
+                    System.out.println("Invalid formula!!!");
+                    break FOR;
+                }           
+            }   
         
+    
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       }       
         return subStringArray;
     }
+    
               
  public static void main(String[] args){  
      
        SubString sc = new SubString();
        sc.arrayMaker();
-       
-        
-       
-       
-       
-     System.out.println(charTypeArray+"--->"+charArray+"======>>"+subStringArray);         
+       ArrayList al = sc.subStringMaker(charTypeArray, charArray);
+             
+     System.out.println("Result ======>> "+subStringArray);         
 }}
